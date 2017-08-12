b7c750b6eaab0b15f920f2cb1b0917ee1f0523a4
akngs <jania902@gmail.com>
2017-08-12 13:20:00 +0900

    Make run.sh to accept multiple params

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -v "/var/run/docker.sock:/var/run/docker.sock" -v "`pwd`:/rootfs/mw" -w "/rootfs/mw" -e PWD="`pwd`" docker/compose:1.15.0 $1
+docker run -v "/var/run/docker.sock:/var/run/docker.sock" -v "`pwd`:/rootfs/mw" -w "/rootfs/mw" -e PWD="`pwd`" docker/compose:1.15.0 $*
